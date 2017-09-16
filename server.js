cf371d50da099c632be6821babae92623aedfd4c
Krisztian Korosi <koeroesi86@gmail.com>
2017-09-16 13:45:09 +0100

    All currency conversion support, and more routes

--- server.js
+++ server.js
@@ -3,34 +3,82 @@ import request from "request";
 
 const app = express();
 const port = 3000;
-request("https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=GBP", function(error, response, body) {
-    body = JSON.parse(body);
-    const firstHit = body[0];
-    const parsed = {
-        symbol: firstHit.symbol,
-        name: firstHit.name,
-        prices: {
-            USD: firstHit.price_usd,
-            GBP: firstHit.price_gbp,
-            BTC: firstHit.price_btc
-        },
-        caps: {
-            USD: firstHit.market_cap_usd,
-            GBP: firstHit.market_cap_gbp
-        }
-    };
-    console.log('firstHit: ', firstHit);
-    console.log('parsed: ', parsed);
-});
+let currentState = {};
+const conversions = ["AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR"].sort();
 
-app.get('/', (req, resp) => {
-    resp.send('Hello from Express!');
+app.set('json spaces', 4);
+
+const updateData = (currentConversions, newState) => {
+    if(!currentConversions.length) {
+        currentState = newState;
+        console.info(`Updated all coins.`);
+        return;
+    }
+
+    if(!newState) {
+        newState = {};
+    }
+
+    const currentCurrency = currentConversions.splice(0, 1);
+
+    request(`https://api.coinmarketcap.com/v1/ticker/?convert=${currentCurrency}`, function(error, response, body) {
+        body = JSON.parse(body);
+        for(let currency of body) {
+            if(!newState[currency.symbol]) {
+                newState[currency.symbol] = {
+                    symbol: currency.symbol,
+                    name: currency.name,
+                    prices: {
+                        USD: currency.price_usd,
+                        BTC: currency.price_btc
+                    },
+                    caps: {
+                        USD: currency.market_cap_usd
+                    },
+                    last_updated: currency.last_updated
+                };
+            }
+
+            newState[currency.symbol].prices[currentCurrency] = currency[`price_${currentCurrency.toString().toLowerCase()}`];
+            newState[currency.symbol].caps[currentCurrency] = currency[`market_cap_${currentCurrency.toString().toLowerCase()}`];
+        }
+
+        console.info(`Updated data for ${currentCurrency}`);
+        setTimeout(() => {
+            updateData(currentConversions, newState);
+        }, 500);
+    });
+};
+
+updateData(conversions);
+setInterval(() => {
+    updateData(conversions);
+}, 480 * 1000);
+
+
+app.get('/coins', (req, resp) => {
+    resp.json(currentState);
+});
+app.get('/coins/list', (req, resp) => {
+    let coins = [];
+    for (let coin in currentState) {
+        coins.push(currentState[coin]);
+    }
+    resp.json(coins);
+});
+app.get('/coins/:coin', (req, resp) => {
+    const coin = req.params.coin;
+    if(currentState[coin]) {
+        resp.json(currentState[coin]);
+    } else {
+        resp.status(404).send(`No data for ${coin}`)
+    }
 });
 
 app.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err);
+        return console.log('Something bad happened', err);
     }
 
-    console.log(`server is listening on ${port}`);
+    console.log(`Server is listening on ${port}`);
 });
